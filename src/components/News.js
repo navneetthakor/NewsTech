936f8d07c25bbf75927d462320154d0b19e82cd8
navneetthakor <tnavneer8628@gmail.com>
2023-04-13 22:41:10 +0530

    next and previous button added

--- src/components/News.js
+++ src/components/News.js
@@ -2,38 +2,83 @@ import React, { Component } from "react";
 import NewsIteam from "./NewsIteam";
 
 export default class News extends Component {
-    state = {
-       article:[],
-       loading:true
-      };
+  state = {
+    article: [],
+    loading: true,
+    page: 1,
+    contry:'in'
+  };
 
-      async componentDidMount()
-      {
-          let url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=05911cada9804e94856bf33224a0d09a";
-          let data = await fetch(url);
-          // console.log(data);
-          let parseData = await data.json();
-          console.log(parseData)
-          this.setState({article:parseData.articles, loading:false})
+  async componentDidMount() {
+    let url =
+      `https://newsapi.org/v2/top-headlines?country=${this.state.contry}&category=technology&apiKey=05911cada9804e94856bf33224a0d09a`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState({ article: parseData.articles, loading: false });
+  }
 
-      }
-    
- 
+  handleprev = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=05911cada9804e94856bf33224a0d09a&page=${
+      this.state.page - 1
+    }`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState({
+      article: parseData.articles,
+      loading: false,
+      page: this.state.page - 1
+    });
+  };
+
+  handlenext = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=05911cada9804e94856bf33224a0d09a&page=${
+      this.state.page + 1
+    }`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState({
+      article: parseData.articles,
+      loading: false,
+      page: this.state.page + 1
+    });
+  };
+
   render() {
-
     return (
       <>
         <div className="container my-3 text-center">
-          <h2 >Leates Tech news for Geeks</h2>
+          <h2>Leates Tech news for Geeks</h2>
           <div className="row">
-          {this.state.article.map((ele)=>{
-           return <div className="col-md-4" key={ele.url}>
-              <NewsIteam title={ele.title.slice(0,20)} disc ={ele.description.slice(0,80)} imgurl = {ele.urlToImage} url={ele.url} />
-            </div>
-          })
-          }
+            {this.state.article.map((ele) => {
+              return (
+                <div className="col-md-4" key={ele.url}>
+                  <NewsIteam
+                    title={ele.title}
+                    disc={ele.description}
+                    imgurl={ele.urlToImage}
+                    url={ele.url}
+                  />
+                </div>
+              );
+            })}
           </div>
 
+          <div className="container d-flex justify-content-evenly">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              onClick={this.handleprev}
+            >
+              &larr; Previous
+            </button>
+            <button
+              type="button"
+              class="btn btn-secondary"
+              onClick={this.handlenext}
+            >
+              Next &rarr;
+            </button>
+          </div>
         </div>
       </>
     );
--- src/components/NewsIteam.js
+++ src/components/NewsIteam.js
@@ -8,7 +8,7 @@ export default class NewsIteam extends Component {
     return (
       <>
         <div className="card my-2 " style={{width: "18rem"}}>
-          <img src={imgurl} className="card-img-top" alt="..." />
+          <img src={imgurl?imgurl:"https://s.yimg.com/uu/api/res/1.2/xZ_X3EY3OdV27CCZB4Mzsw--~B/Zmk9ZmlsbDtoPTYzMDtweW9mZj0wO3c9MTIwMDthcHBpZD15dGFjaHlvbg--/https://media-mbst-pub-ue1.s3.amazonaws.com/creatr-uploaded-images/2023-04/d260ac20-d92d-11ed-bcbf-d1eb56be0f45.cf.jpg"} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">
