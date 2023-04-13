e04851e4a6b8a86271dee25c192f65c6ff9c5af7
navneetthakor <tnavneer8628@gmail.com>
2023-04-13 16:07:32 +0530

    grid added + samplejson created

--- src/components/News.js
+++ src/components/News.js
@@ -1,18 +1,50 @@
-import React, { Component } from 'react'
-import NewsIteam from './NewsIteam'
+import React, { Component } from "react";
+import NewsIteam from "./NewsIteam";
 
 export default class News extends Component {
   render() {
     return (
       <>
-      <h2>this is news Component</h2>
-      <NewsIteam/>
-      <NewsIteam/>
-      <NewsIteam/>
-      <NewsIteam/>
-      <NewsIteam/>
-      <NewsIteam/>
+        <div className="container my-3 text-center">
+          <h2 >KalTak news station</h2>
+
+          <div className="row">
+            <div className="col-md-4">
+              <NewsIteam />
+            </div>
+            <div className="col-md-4">
+              <NewsIteam />
+            </div>
+            <div className="col-md-4">
+              <NewsIteam />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-4">
+              <NewsIteam />
+            </div>
+            <div className="col-md-4">
+              <NewsIteam />
+            </div>
+            <div className="col-md-4">
+              <NewsIteam />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-4">
+              <NewsIteam />
+            </div>
+            <div className="col-md-4">
+              <NewsIteam />
+            </div>
+            <div className="col-md-4">
+              <NewsIteam />
+            </div>
+          </div>
+        </div>
       </>
-    )
+    );
   }
 }
--- src/components/NewsIteam.js
+++ src/components/NewsIteam.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+// import logo from '../../public/logo192.png'
 
 export default class NewsIteam extends Component {
   render() {
     return (
-      <div>
-        <h4>News iteam</h4>
-      </div>
-    )
+      <>
+        <div className="card my-2 " style={{width: "18rem"}}>
+          <img src="https://photos5.appleinsider.com/gallery/53945-108525-Cupertino-xl.jpg" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="/" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </>
+    );
   }
 }
