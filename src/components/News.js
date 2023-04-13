62e3304bade57869cf69cf2fb2342ab4894043f1
navneetthakor <tnavneer8628@gmail.com>
2023-04-13 14:25:47 +0530

    news & newsiteam with navbar added 1s

---
+++ src/components/News.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import NewsIteam from './NewsIteam'
+
+export default class News extends Component {
+  render() {
+    return (
+      <>
+      <h2>this is news Component</h2>
+      <NewsIteam/>
+      <NewsIteam/>
+      <NewsIteam/>
+      <NewsIteam/>
+      <NewsIteam/>
+      <NewsIteam/>
+      </>
+    )
+  }
+}
---
+++ src/components/NewsIteam.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NewsIteam extends Component {
+  render() {
+    return (
+      <div>
+        <h4>News iteam</h4>
+      </div>
+    )
+  }
+}
